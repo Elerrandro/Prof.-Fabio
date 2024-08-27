6293f5494e9e5b6df5780c62698c2fbb01346959
Elerrandro <elerrandrosousa@gmail.com>
2024-08-27 16:03:29 -0300

    Update TABUADA.py

--- TABUADA.py
+++ TABUADA.py
@@ -1,6 +1,21 @@
+import os
+#FUNÇÃO PARA LIMPAR TELA
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 print("MENU")
 print("ESCOLHA ENTRE AS OPÇÕES ABAIXO")
-print("1 - Qual a operação que você quer usar? (+, -, *, /)")
-print("2 - Tabuada Completa! (+, -, *, /)")
+print("1 - Escolha a operação e o número (+, -, *, /)")
+print("2 - Escolha somente a operação, Tabuada Completa! (+, -, *, /)")
 print("3 - SAIR")
 opcao = int(input("Digite aqui o número: "))
+
+limpar_tela() #VAI LIMPAR A TELA ANTES DE CONTINUAR
+
+if opcao == 1:
+    print("Qual operação vai querer utilizar?")
+    print("Escolha entre as seguintes opções")
+    print("(+, -, *, /)")
+    operacao = str(input("Digite aqui: "))
+    numero = int(input("Qual o número da tabuada você quer: "))
